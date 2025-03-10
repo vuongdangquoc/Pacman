0a422d9358877124294e6de2edb994634bff9e9c
DeokBae103 <ducbm1003@gmail.com>
2025-03-10 21:04:21 +0700

    Gate + wall diamond

---
+++ Assets/Gate.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public Transform connection;
+    public GameObject gameObject;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            Vector3 position = collision.transform.position;
+            position.x = connection.position.x;
+            position.y = connection.position.y;
+
+            collision.transform.position = position;
+            Destroy(gameObject);
+            Destroy(connection.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/GateController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class GateController : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.SetActive(false);
+        // Bắt đầu Coroutine khi scene chạy
+        StartCoroutine(ActiveAfterDelay());
+    }
+
+    public IEnumerator ActiveAfterDelay()
+    {
+        // Đợi giây
+        yield return new WaitForSeconds(10f);
+        // Kích hoạt GameObject
+        gameObject.SetActive(true);
+    }
+}
